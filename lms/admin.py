4f6bbea86b7d3627209ea14a0ad9cbf2ed59ef8f
ShubbzG <shubhankargosain@gmail.com>
2025-11-08 15:37:54 +0530

    Revert "Merge branch 'main' of https://github.com/AbhirajBhatta/DBMS_LMS_Project"
    This reverts commit 8de628623547ed157a2b057d2b7089e5cbae24d9, reversing
    changes made to 98d219a683c8e1c0002cf2e324bf92b1a2acf9b3.

--- lms/admin.py
+++ lms/admin.py
@@ -1,11 +1,3 @@
 from django.contrib import admin
-from .models import Course, Assignment, Attendance, Grade, Quiz
-
-admin.site.register(Course)
-admin.site.register(Assignment)
-admin.site.register(Attendance)
-admin.site.register(Grade)
-admin.site.register(Quiz)
-
 
 # Register your models here.
