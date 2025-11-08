1dcabf8400cc9d989fe03ca81d74041f620e8873
AbhirajBhatta <abhirajbhatta@gmail.com>
2025-11-08 15:01:43 +0530

    basic UI is made

--- lms/views.py
+++ lms/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from .models import Assignment, Attendance, Grade, Quiz
+
+def dashboard(request):
+    assignments = Assignment.objects.all()
+    attendance = Attendance.objects.all()
+    grades = Grade.objects.all()
+    quizzes = Quiz.objects.all()
+
+    context = {
+        'assignments': assignments,
+        'attendance': attendance,
+        'grades': grades,
+        'quizzes': quizzes,
+    }
+
+    return render(request, 'lms/dashboard.html', context)
 
 # Create your views here.
