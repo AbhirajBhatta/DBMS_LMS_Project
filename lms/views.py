98d219a683c8e1c0002cf2e324bf92b1a2acf9b3
ShubbzG <shubhankargosain@gmail.com>
2025-11-08 11:26:01 +0530

    trying out frontend rn

--- lms/views.py
+++ lms/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from .models import Course
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
+if Users.objects.filter(username=u,password=p).exists():
+    return redirect("main")
+
+@login_required
+def main(request):
+    courses = Course.objects.all()   # later we can filter by user if needed
+    return render(request, 'main.html', {'courses': courses})
--- lms_project/urls.py
+++ lms_project/urls.py
@@ -17,6 +17,13 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+path("",views.login_page,name="login"),
+path("main/",views.main_page,name="main"),
+
+from django.contrib.auth import views as auth_views
+
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('', main, name='main'),
 ]
